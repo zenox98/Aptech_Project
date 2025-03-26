b63b096584ac1a0f9f84666ec27204a917340ef2
zenox98 <zononxshield2002@gmail.com>
2025-03-26 15:11:20 +0530

    replaced apis folder with routes/apis folder structure

--- backend/src/apis/post/postExcelData.ts
+++
@@ -1,7 +0,0 @@
-import e, {Router, Request, Response} from "express";
-
-// database
-import db from '../../db.ts'
-
-
-const router
--- backend/src/apis/products/type.ts
+++
@@ -1,4 +0,0 @@
-export interface ProductProps {
-  name : string,
-  amount : number
-}
